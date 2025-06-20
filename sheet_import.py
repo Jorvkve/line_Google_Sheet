fb06ec189074b0ce2d81cbff64ae2432d3427751
Jorvkve <Turth00@gmail.com>
2025-06-20 13:21:51 +0700

    update final code

--- sheet_import.py
+++
@@ -1,8 +0,0 @@
-import gspread 
-import pandas as pd
-import schedule
-import time
-from oauth2client.service_account import ServiceAccountCredentials
-from linebot.v3.messaging import MessagingApi, PushMessageRequest, TextMessage, Configuration, FileMessage
-from linebot.v3.messaging.api_client import ApiClient
-from datetime import datetime, timedelta
