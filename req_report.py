543ea7cfc8222f42b14793b143fa6efe38bb217c
Jorvkve <Turth00@gmail.com>
2025-07-01 16:21:49 +0700

    try telegram

--- req_report.py
+++ req_report.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime, timedelta
 
-WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbx-PCV-lF84GN1Y4rlaY4ilHPiwWmqaPEwZQtWFtJkVr92zMFgRbS2HIf2pl37xC0nC/exec'  # ใส่ Web App URL
-SECRET_KEY = 'SentReport01'  # ใส่ secret key ที่คุณตั้งใน Google Apps Script
-DATE_TO_SEND = (datetime.now() - timedelta(days=0)).strftime('%#d/%#m/%Y')  # ใช้วันที่ของเมื่อวาน 0 = ปัจจุบัน เป็น + คือเพิ่มวันในอนาคต
+WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby7dJRANIxK7w6hzoGfQCw7q2QwyxC53U4D-ATbt8gOVbFNx_cOERJ0nF6JAt3fk31P/exec'  # ใส่ Web App URL
+SECRET_KEY = 'SentReportTelegram' #'SentReport01' # ใส่ secret key ที่คุณตั้งใน Google Apps Script
+DATE_TO_SEND = (datetime.now() - timedelta(days=14)).strftime('%#d/%#m/%Y')  # ใช้วันที่ของเมื่อวาน 0 = ปัจจุบัน เป็น + คือเพิ่มวันในอนาคต
 
 params = {
     'key': SECRET_KEY,
@@ -16,3 +16,4 @@ if response.status_code == 200:
     print("เรียกใช้ Apps Script สำเร็จ:", response.text)
 else:
     print("เกิดข้อผิดพลาด:", response.status_code, response.text)
+    
