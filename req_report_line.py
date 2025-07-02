f1fb086b3202a3b6c3c58fba361d423a01980fa0
Jorvkve <Turth00@gmail.com>
2025-07-02 16:15:08 +0700

    update

--- req_report_line.py
+++ req_report_line.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyZSRHIl1IJZsPqFicJLmFAiRBvq-hzYxoLmfTteLU2ej2KzE5zkxeAVy83ZDaPDTxU/exec'  # ใส่ Web App URL
 SECRET_KEY = 'SentReport01' #'SentReportTelegram' #'SentReport01' # ใส่ secret key ที่คุณตั้งใน Google Apps Script
-DATE_TO_SEND = (datetime.now() - timedelta(days=13)).strftime('%#d/%#m/%Y')  # ใช้วันที่ของเมื่อวาน 0 = ปัจจุบัน เป็น + คือเพิ่มวันในอนาคต
+DATE_TO_SEND = (datetime.now() - timedelta(days=1)).strftime('%#d/%#m/%Y')  # ใช้วันที่ของเมื่อวาน 0 = ปัจจุบัน เป็น + คือเพิ่มวันในอนาคต
 
 params = {
     'key': SECRET_KEY,
